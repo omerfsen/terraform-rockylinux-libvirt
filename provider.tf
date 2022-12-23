aeb20894f6cc2f42f9fc9d89eab457c27e89f6bf
omerfsen <omerfsen@gmail.com>
2022-12-23 20:46:07 +0000

    terraform version

--- provider.tf
+++ provider.tf
@@ -1,11 +1,15 @@
 terraform {
+  required_version = ">= 1.3.6"
+
   required_providers {
     libvirt = {
       source  = "dmacvicar/libvirt"
       version = "0.7.0"
     }
   }
+
 }
+
 
 provider "libvirt" {
   #uri = "qemu:///system"
