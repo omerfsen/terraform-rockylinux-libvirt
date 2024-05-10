3264b62a05626e11eec9b4d80bc613b24d9f2f35
OmerSen <omerfsen@gmail.com>
2024-05-10 09:46:28 +0100

    Upgrade providers

--- provider.tf
+++ provider.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = "= 1.5.7"
+  required_version = "= 1.8.3"
 
   required_providers {
     libvirt = {
       source  = "dmacvicar/libvirt"
-      version = "0.7.1"
+      version = "0.7.6"
     }
   }
 
